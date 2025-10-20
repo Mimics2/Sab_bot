0d8d3fcbcf433f1e4958b57b8f6a80d52b532f46
Mimics2 <kolesnicenkauoevsej@gmail.com>
2025-10-20 11:04:16 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE')
-ADMIN_IDS = [int(x) for x in os.getenv('ADMIN_IDS', '123456789').split(',')]
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7557745613:AAFTpWsCJ2bZMqD6GDwTynnqA8Nc-mRF1Rs')
+ADMIN_IDS = [int(x) for x in os.getenv('ADMIN_IDS', '6646433980').split(',')]
 
 # Настройки базы данных
 DATABASE_URL = "sqlite:///bot.db"
